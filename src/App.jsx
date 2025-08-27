f4e1bfc9476d2c50f8fa9461f3d0b8ea9d302df1
Jyoti-sharma <jyo.sharma2505@gmail.com>
2025-08-27 12:52:51 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Meets from "./components/Meets";
 import "./index.css";
-
+import { useState } from "react";
 
 function App() {
-
+  const [title,setTitle]=useState("")
   return (
     <>
-      <Header  />
-      <Meets />
+      <Header setTitle = {setTitle} />
+      <Meets title = {title}/>
     </>
   );
 }
