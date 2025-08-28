a7cf93bfdb457c8808d032a47ad7c28db7285322
Jyoti-sharma <jyo.sharma2505@gmail.com>
2025-08-28 10:06:25 +0530

    fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-// import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 // import useFetch from "../useFetch";
 import { useState } from "react";
 
@@ -15,6 +15,7 @@ const Header = ({setTitle}) => {
     <header>
       <nav class="navbar navbar-light bg-grey">
         <div class="container-fluid container">
+          <Link to="/">
           <img
             src="https://cdn.worldvectorlogo.com/logos/google-meet-2.svg"
             height="70px"
@@ -22,6 +23,7 @@ const Header = ({setTitle}) => {
             style={{ backgroundColor: "transparent" }}
             // style={{ backgroundColor: "none" }}
           />
+          </Link>
           <form class="d-flex" onSubmit={handleSubmit}>
             <input
               class="form-control me-2"
